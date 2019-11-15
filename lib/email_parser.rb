e444fc8efc7624e375573d87fbeba494be466444
Anna Wijetunga <annawijetunga@gmail.com>
2019-11-15 10:26:38 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -24,14 +24,16 @@
 # end 
 
 class EmailParser
-  attr_accessor :emails
 
-  def initialize(email)
-    @emails = email
+  attr_accessor :email
+  def  initialize(emails)
+    @email = emails
   end
 
   def parse
-    emails.delete(',').split.uniq
+    email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
   end
 
 end
